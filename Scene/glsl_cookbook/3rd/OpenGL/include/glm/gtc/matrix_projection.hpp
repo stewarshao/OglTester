21e1d023eeadaa647e1943fc030389afc3074811
ironman7238 <ironman7238@126.com>
2012-11-22 17:31:58 +0800

    remove src

--- Scene/glsl_cookbook/3rd/OpenGL/include/glm/gtc/matrix_projection.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef GLM_GTC_matrix_projection
-#define GLM_GTC_matrix_projection GLM_VERSION
-
-
-#endif
--- UiFrame/inc/globaldef.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GLOBAL_DEF__
-#define __GLOBAL_DEF__
-
-typedef unsigned int GlwHandle;			 /**< gl´°¿ÚµÄ¾ä±ú */
-
-
-
-
-#endif	//__GLOBAL_DEF__
